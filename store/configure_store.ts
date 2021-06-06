0ac7db226eb208c20c4c12b7cdd671f8d3ef48b1
nohseungrae <ca10205@naver.com>
2021-06-07 00:01:03 +0900

    saga init

--- store/configure_store.ts
+++ store/configure_store.ts
@@ -3,6 +3,15 @@ import { AnyAction, applyMiddleware, compose, createStore, Store } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "../reducers";
 // import thunkMiddleware from "redux-thunk";
+import createSaraMiddleware from "redux-saga";
+import "redux";
+import { Task } from "redux-saga";
+
+declare module "redux" {
+  export interface Store {
+    sagaTask?: Task;
+  }
+}
 
 const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
   console.log(action);
@@ -15,13 +24,15 @@ const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
 //히스토리가 쌓이면 메모리도 많이 먹고 중앙데이터가 다 보이기 때문에 보안에 취약할 수 있으므로 다르게 적용
 const configureStore: MakeStore<Store<any, AnyAction>> = () => {
   //리덕스에 기능을 추가해주는!!
-  const middlewares = [loggerMiddleware];
+  const sagaMiddleware = createSaraMiddleware();
+  const middlewares = [sagaMiddleware, loggerMiddleware];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(applyMiddleware(...middlewares));
 
-  const store = createStore(rootReducer, enhancer);
+  const store: Store = createStore(rootReducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
